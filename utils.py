810dc96f06ce9e5e72b2df90c8cfe7d083b5db24
adil <adilkhan49@hotmail.com>
2024-05-21 22:08:21 +0100

    use env instead of credentials

--- utils.py
+++ utils.py
@@ -1,8 +1,8 @@
 import gspread
 import pandas as pd
 
-def get_data(CREDENTIALS,SPREADSHEET_ID,WORKSHEET_NAME,TGT_CSV_FILENAME):
-    gc = gspread.service_account(filename=CREDENTIALS)
+def get_data(CONFIG,SPREADSHEET_ID,WORKSHEET_NAME,TGT_CSV_FILENAME):
+    gc = gspread.service_account_from_dict(CONFIG)
     sh = gc.open_by_key(SPREADSHEET_ID)
     ws = sh.worksheet(WORKSHEET_NAME)
     df = pd.DataFrame(ws.get_all_records())
@@ -11,8 +11,10 @@ def get_data(CREDENTIALS,SPREADSHEET_ID,WORKSHEET_NAME,TGT_CSV_FILENAME):
     return df
 
 if __name__ == '__main__':
-    CREDENTIALS = 'credentials.json'
+    from dotenv import dotenv_values
+    CONFIG = dotenv_values(".env")
+    CONFIG["private_key"] = CONFIG["private_key"].replace("||n||", "\n")
     SPREADSHEET_ID="1XVVdtHa9h10QZ1hoQ2gh5HGywcgT7X61UhgiI9-6JrY"
     WORKSHEET_NAME = 'Pops'
     TGT_CSV_FILENAME = 'Pop Sheet - Pops.csv'
-    get_data(SPREADSHEET_ID,WORKSHEET_NAME,TGT_CSV_FILENAME)
+    get_data(CONFIG,SPREADSHEET_ID,WORKSHEET_NAME,TGT_CSV_FILENAME)
